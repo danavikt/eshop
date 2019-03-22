d4e0abd326d81fb3ad58d4f3556684e23b3e7e94
danavikt <viktorija.danaityte@gmail.com>
2019-03-22 21:18:45 +0200

    first commit

---
+++ src/auth/actions.js
@@ -0,0 +1,5 @@
+import * as types from "./actionTypes";
+
+export const login = () => ({ type: types.LOGIN });
+
+export const logout = () => ({ type: types.LOGOUT });
---
+++ src/shop/reducer.js
@@ -0,0 +1,40 @@
+import { toggleFavoriteById, updateCartCountById } from "./utils";
+import * as types from "./actionTypes";
+
+const DEFAULT_STATE = {
+  products: [],
+  error: null,
+  loading: false,
+};
+
+export default (state = DEFAULT_STATE, action) => {
+  if (action.type === types.FETCH_PRODUCTS_FAILURE) {
+    console.log(action.payload);
+  }
+
+  switch (action.type) {
+    case types.FETCH_PRODUCTS:
+      return { ...state, loading: true };
+    case types.FETCH_PRODUCTS_SUCCESS:
+      return { ...state, loading: false, products: action.payload };
+    case types.FETCH_PRODUCTS_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+
+    case types.TOGGLE_FAVORITE_PRODUCT:
+      return {
+        ...state,
+        products: toggleFavoriteById(state.products, action.payload),
+      };
+
+    case types.UPDATE_PRODUCT_CART_COUNT: {
+      const { id, count } = action.payload;
+      return {
+        ...state,
+        products: updateCartCountById(state.products, id, count),
+      };
+    }
+
+    default:
+      return state;
+  }
+};
